436a335358d5376fb973e7fa3b73619566d1fd03
Ciro Di Marsico <cirodimarsico07@gmail.com>
2026-08-11 22:34:15 -0300

    a

--- my-app/src/components/Product.jsx
+++ my-app/src/components/Product.jsx
@@ -82,7 +82,7 @@ const Product = ({
 
   return (
     <div className="w-[170px] max-[370px]:w-[150px] bg-[#1E1E1E] min-h-[300px] rounded-3xl flex flex-col items-center justify-between p-3 font-['prompt'] relative overflow-hidden">
-      {totalStock <= 2 && totalStock > 0 && product.rubro === "bebidas" &&
+      {totalStock <= 2 && totalStock > 0 && ( product.category_id == 5 || product.category_id == 6 || product.category_id == 7 || product.category_id == 8 || product.category_id == 9 || product.category_id == 10 || product.category_id == 13) &&
         <span className="text-[14px] text-white leading-none font-bold flex flex-col items-center justify-center bg-red-500/70 px-2 w-[180px] h-[34px] absolute right-[-57px] top-[10px] rotate-[35deg]">
           <p>¡ULTIMAS</p>
           <p>UNIDADES!</p>
@@ -91,7 +91,7 @@ const Product = ({
       
       {
       //20 off
-      product.rubro !== "bebidas" &&
+        ( product.category_id == 3 || product.category_id == 4 ||product.category_id == 11 || product.category_id == 12) &&
         <span className="text-[14px] text-white leading-none font-bold flex flex-col items-center justify-center bg-red-500/70 px-2 w-[180px] h-[34px] absolute right-[-57px] top-[10px] rotate-[35deg]">
           <p>20% OFF</p>
         </span>
