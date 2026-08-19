e2a5517abdccb99fbd34825f76cfec55f29816c1
Ciro Di Marsico <cirodimarsico07@gmail.com>
2026-08-19 10:35:11 -0300

    dividir

--- my-app/src/components/Cart.jsx
+++ my-app/src/components/Cart.jsx
@@ -182,9 +182,9 @@ const Cart = ({ carrito = [], data, promos = [], agregarAlCarrito, agregarPromoA
           })}
         </div>
 
-        <div className="px-6 pb-8 pt-4 border-t border-white/10">
-          <a className="font-[prompt] text-[20px] text-center text-white/90">¿estas con amigos?</a>
-          <h4 className="font-[prompt] text-[14px] text-center text-white/80">dividi el total</h4>
+        <div className="px-6 pb-4 pt-4 border-t border-white/10">
+          <a className="font-[prompt] text-[20px] text-center text-white/90 font-bold">¿ESTAS CON AMIGOS?</a>
+          <h4 className="font-[prompt] text-[14px] text-center text-white/80 pb-4">DIVIDI EL TOTAL</h4>
           <div className="border-t border-white/20 mb-4" />
           <div className="flex justify-between items-center">
             <span className="font-['koulen'] text-[26px] tracking-widest">TOTAL:</span>
